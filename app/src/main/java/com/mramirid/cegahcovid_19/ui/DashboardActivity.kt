5e6d46baf747320006a548d0660ac4f5789a8f2d
Amir <amir.hakim@kolu.web.id>
2020-03-28 23:02:56 +0700

    Add menu

--- app/src/main/java/com/mramirid/cegahcovid_19/ui/DashboardActivity.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/DashboardActivity.kt
@@ -1,6 +1,10 @@
 package com.mramirid.cegahcovid_19.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,8 +24,8 @@ class DashboardActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_checkup,
@@ -29,5 +33,22 @@ class DashboardActivity : AppCompatActivity() {
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.credit_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val moveToWebsite = when(item.itemId) {
+            R.id.source_covid19 -> Intent(Intent.ACTION_VIEW, Uri.parse("https://www.alodokter.com/virus-corona"))
+            R.id.source_api -> Intent(Intent.ACTION_VIEW, Uri.parse("https://kawalcorona.com/api/"))
+            else -> Intent(this, DashboardActivity::class.java)
+        }
+
+        startActivity(moveToWebsite)
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.mramirid.cegahcovid_19.R
 
 class StatisticsFragment : Fragment() {
@@ -15,17 +12,10 @@ class StatisticsFragment : Fragment() {
     private lateinit var notificationsViewModel: StatisticsViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(StatisticsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_statistics, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
     }
 }
