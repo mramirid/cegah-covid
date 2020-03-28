15b5da4fefa4ccb857f31b4c41395ec113399903
Amir <amir.hakim@kolu.web.id>
2020-03-28 15:44:54 +0700

    Add header

--- app/src/main/java/com/mramirid/cegahcovid_19/ui/DashboardActivity.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/DashboardActivity.kt
@@ -14,11 +14,14 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        supportActionBar?.elevation = 0F
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_checkup,
--- app/src/main/java/com/mramirid/cegahcovid_19/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mramirid.cegahcovid_19.R
 
@@ -22,10 +20,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
     }
 }
