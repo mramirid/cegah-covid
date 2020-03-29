e4c06fc4d371ddbdb8c99a1bdaa0ac71586fc892
Amir <amir.hakim@kolu.web.id>
2020-03-29 10:48:00 +0700

    Add statistik

--- app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsFragment.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.mramirid.cegahcovid_19.R
+import kotlinx.android.synthetic.main.fragment_statistics.*
 
 class StatisticsFragment : Fragment() {
-
-    companion object {
-        private const val URL_API = "https://api.kawalcorona.com/indonesia/"
-    }
 
     private val statisticsViewModel by viewModels<StatisticsViewModel>()
 
@@ -27,6 +26,17 @@ class StatisticsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        statisticsViewModel.fetchStatus()
+        statisticsViewModel.getStatusCovidId().observe(viewLifecycleOwner, Observer { statusCovid ->
+            tv_total_positive.text = statusCovid.positif
+            tv_total_recovered.text = statusCovid.sembuh
+            tv_total_death.text = statusCovid.meninggal
+        })
 
+        statisticsViewModel.getStatusRequest().observe(viewLifecycleOwner, Observer { hasReceived ->
+            if (!hasReceived) {
+                Toast.makeText(context, "Tidak dapat terhubung ke internet, coba lagi nanti", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/mramirid/cegahcovid_19/ui/statistics/StatisticsViewModel.kt
@@ -1,13 +1,63 @@
 package com.mramirid.cegahcovid_19.ui.statistics
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.mramirid.cegahcovid_19.model.StatusCovid
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
 
 class StatisticsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    companion object {
+        private const val URL_API = "https://api.kawalcorona.com/indonesia/"
     }
-    val text: LiveData<String> = _text
+
+    private val statusCovidId = MutableLiveData<StatusCovid>()
+    private val hasReceived = MutableLiveData<Boolean>()
+
+    fun fetchStatus() {
+        AsyncHttpClient().get(URL_API, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                val result = String(responseBody!!)
+                val responseArray = JSONArray(result)
+
+                for (i in 0 until responseArray.length()) {
+                    val responseObject = responseArray.getJSONObject(i)
+                    val statusCovid = StatusCovid(
+                        responseObject.getString("name"),
+                        responseObject.getString("positif"),
+                        responseObject.getString("sembuh"),
+                        responseObject.getString("meninggal")
+                    )
+                    statusCovidId.postValue(statusCovid)
+                }
+
+                hasReceived.postValue(true)
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                hasReceived.postValue(false)
+            }
+
+        })
+    }
+
+    fun getStatusCovidId(): MutableLiveData<StatusCovid> {
+        return statusCovidId
+    }
+
+    fun getStatusRequest(): MutableLiveData<Boolean> {
+        return hasReceived
+    }
 }
